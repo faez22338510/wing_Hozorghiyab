6bc550daa785359d3d8f2c7a7281d77ad088ec68
faez22338510 <faez.ham@gmail.com>
2017-05-04 16:50:17 +0430

    Update Login.js

--- Component/LoginPage/Login.js
+++ Component/LoginPage/Login.js
@@ -1,48 +1,66 @@
 /**
  * Created by HRFaez on 09/04/2017.
  */
-import React ,{ Component } from 'react';
+import React, {Component} from 'react';
 import {
     Text,
     View,
     Button,
     Navigator,
     StyleSheet,
-    BackAndroid,
     TextInput,
+    TouchableWithoutFeedback,
+    AsyncStorage
 } from 'react-native';
-
-class Login extends Component{
-    constructor(props){
+import {Keyboard} from 'react-native';
+class Login extends Component {
+    constructor(props) {
         super(props);
-        this.state={
-            id: null,
+        this.state = {
+            //Employeeid: "",
         }
         this.onOrgPress = this.onOrgPress.bind(this);
     }
-    onOrgPress(){
-        console.log("FirstPage");
-        this.props.navigator.push({
-            id: 2,
-            orgId:this.state.id
-        })
+
+    componentDidMount() {
+        AsyncStorage.getItem("Employeeid").then((value) => {
+            this.setState({"Employeeid": value});
+        }).done();
     }
-    render(){
-        return(
+    saveData(value) {
+        AsyncStorage.setItem("Employeeid", value);
+        this.setState({"Employeeid": value});
+    }
+
+    onOrgPress() {
+        if (this.state.Employeeid != "") {
+            this.props.navigator.push({
+                id: 2,
+                orgId: this.state.Employeeid
+            })
+        }
+    }
+    getInitialState() {
+        return {};
+    }
+    render() {
+        return (
             <View style={{flex:1}}>
-                <View style={{flex:2}}></View>
+                <View style={{flex:2}}>
+                </View>
                 <View
                     style={{flex:1,paddingRight:30,paddingLeft:30,flexDirection: 'row'}}
                 >
-                    <View style={{flex:1}}></View>
+                    <View style={{flex:1}}>
+                    </View>
                     <View style={{flex:3}}>
-                    <TextInput
-                        keyboardType={'numeric'}
-                        style={{height: 40,textAlign: 'center',fontSize:16}}
-                        placeholder="کد کارمندی خود را وارد کنید"
-                        onChangeText={(id) => this.setState({id})}
-
-                    />
+                        <TextInput
+                            keyboardType={'numeric'}
+                            style={{height: 40,textAlign: 'center',fontSize:16,borderColor: 'gray', borderWidth: 1}}
+                            placeholder="کد کارمندی خود را وارد کنید"
+                            onChangeText={(text) => this.saveData(text)}
+                            value={this.state.Employeeid}
+                        />
                     </View>
                     <View style={{flex:1}}></View>
                 </View>
@@ -65,7 +83,37 @@ class Login extends Component{
 }
 
 const styles = StyleSheet.create({
+    container: {
+        padding: 30,
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "stretch",
+        backgroundColor: "#F5FCFF",
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    formInput: {
+        flex: 1,
+        height: 26,
+        fontSize: 13,
+        borderWidth: 1,
+        borderColor: "#555555",
+    },
+    saved: {
+        fontSize: 20,
+        textAlign: "center",
+        margin: 10,
+    },
+    instructions: {
+        textAlign: "center",
+        color: "#333333",
+        marginBottom: 5,
+        marginTop: 5,
+    },
+});
 
-})
 
 module.exports = Login;
